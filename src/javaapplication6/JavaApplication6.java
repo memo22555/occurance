499731d7d6292c1dc22ee2aa4f716b5ffa6035f4
memo22555 <memoyasser22555@gmail.com>
2021-04-16 19:38:21 -0700

    secound edit

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -15,20 +15,22 @@ public class JavaApplication6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-String s="abcabcabc";
+        String s = "abcabcabcabcabcabcabcabcabcabc";
         String Sample = "abc";
-int n=0;
-int position=0;
-for(int i=0;true;i++){
-   int result= s.indexOf(Sample, position);
-    if (result !=-1){
-        n++;
-        position+=3;
-      
-    }else
-        break;
-}
-        System.out.println(n);
+        //System.out.println(s.length());
+        char C[] = s.toCharArray();
+        // System.out.println(C[0]);
+        int n = 15;
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (C[i] == 'a' && C[i + 1] == 'b' && 'c' == C[i + 2]) {
+                count++;
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        System.out.println(count);
+
     }
-    
 }
